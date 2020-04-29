6a6c126a8aec176a87755addf478368aca2043a2
Stiverson <Stiverson.machado@gmail.com>
2020-04-29 15:36:48 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/stiverson/projeto/cursomc/CursoMcApplication.java
+++ src/main/java/com/stiverson/projeto/cursomc/CursoMcApplication.java
@@ -71,6 +71,11 @@ public class CursoMcApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Games");
+		Categoria cat3 = new Categoria(null,"SmartPhones");
+		Categoria cat4 = new Categoria(null,"Smartwatches");
+		Categoria cat5 = new Categoria(null,"Periféricos");
+		Categoria cat6 = new Categoria(null,"Consoles");
+		Categoria cat7 = new Categoria(null,"Cadeiras Gamers");
 		
 		Produto p1 = new Produto (null,"Notebook", 2500.00);
 		Produto p2 = new Produto (null,"Impressora", 800.00);
@@ -83,7 +88,7 @@ public class CursoMcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1));
 		p3.getCategorias().addAll(Arrays.asList(cat2));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2,cat3,cat4,cat5,cat6,cat7));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		Estado est1 = new Estado(null,"Minas Gerais");
