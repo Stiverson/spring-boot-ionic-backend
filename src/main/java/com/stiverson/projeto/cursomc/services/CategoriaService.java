869ece184fb5c2afabdbf1653630993c633b7fb9
Stiverson <Stiverson.machado@gmail.com>
2020-04-29 12:37:31 -0300

    Habilitando o delete da categoria e tratamento de deleção para categorias com produtos

--- src/main/java/com/stiverson/projeto/cursomc/services/CategoriaService.java
+++ src/main/java/com/stiverson/projeto/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.stiverson.projeto.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.stiverson.projeto.cursomc.domain.Categoria;
 import com.stiverson.projeto.cursomc.repositories.CategoriaRepository;
+import com.stiverson.projeto.cursomc.services.exceptions.DataIntegrityException;
 import com.stiverson.projeto.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +32,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete (Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
 	}
 }
