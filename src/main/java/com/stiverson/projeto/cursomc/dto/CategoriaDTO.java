e2c611bf9a271cd7995aaa844d2f068dffa395da
Stiverson <Stiverson.machado@gmail.com>
2020-04-30 10:49:02 -0300

    Validação sintática com bean validation

--- src/main/java/com/stiverson/projeto/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/stiverson/projeto/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.stiverson.projeto.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.stiverson.projeto.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -10,6 +14,9 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento do campo é obrigatório")
+	@Length(min=5 , max=80, message="O nome do campo tem que ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/stiverson/projeto/cursomc/services/CategoriaService.java
+++ src/main/java/com/stiverson/projeto/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.stiverson.projeto.cursomc.domain.Categoria;
+import com.stiverson.projeto.cursomc.dto.CategoriaDTO;
 import com.stiverson.projeto.cursomc.repositories.CategoriaRepository;
 import com.stiverson.projeto.cursomc.services.exceptions.DataIntegrityException;
 import com.stiverson.projeto.cursomc.services.exceptions.ObjectNotFoundException;
@@ -58,5 +59,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		
+		return new Categoria(objDto.getId(),objDto.getNome());
 	}
 }
