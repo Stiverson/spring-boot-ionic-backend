9b2a22093290fa9a98608ec6546b3cd8f35531c9
Stiverson <Stiverson.machado@gmail.com>
2020-05-04 13:14:04 -0300

    Inserção de novos clientes via post

--- src/main/java/com/stiverson/projeto/cursomc/services/ClienteService.java
+++ src/main/java/com/stiverson/projeto/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.stiverson.projeto.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.stiverson.projeto.cursomc.domain.Cidade;
 import com.stiverson.projeto.cursomc.domain.Cliente;
+import com.stiverson.projeto.cursomc.domain.Endereco;
+import com.stiverson.projeto.cursomc.domain.enums.TipoCliente;
 import com.stiverson.projeto.cursomc.dto.ClienteDTO;
+import com.stiverson.projeto.cursomc.dto.ClienteNewDTO;
+import com.stiverson.projeto.cursomc.repositories.CidadeRepository;
 import com.stiverson.projeto.cursomc.repositories.ClienteRepository;
+import com.stiverson.projeto.cursomc.repositories.EnderecoRepository;
 import com.stiverson.projeto.cursomc.services.exceptions.DataIntegrityException;
 import com.stiverson.projeto.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -22,11 +30,25 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
 	
 
 	public Cliente update(Cliente obj) {
@@ -60,6 +82,28 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDto) {
 		
 		return new Cliente(objDto.getId(), objDto.getNome(),objDto.getEmail(),null,null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(),objDto.getCpfOuCnpj(),TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(),null,null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		
+		if(objDto.getTelefone2()!= null) {
+			
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		
+		if(objDto.getTelefone3()!= null) {
+			
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj , Cliente obj) {
