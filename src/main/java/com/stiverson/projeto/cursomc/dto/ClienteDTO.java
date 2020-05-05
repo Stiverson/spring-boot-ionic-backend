62c4406eabddff4ac82c8ea891ca29c427760cc8
Stiverson <stiverson.machado@gmail.com>
2020-05-05 15:16:05 -0300

    Validação customizada de e-mail não repetido na atualização de cliente

--- src/main/java/com/stiverson/projeto/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/stiverson/projeto/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.stiverson.projeto.cursomc.domain.Cliente;
+import com.stiverson.projeto.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	
 	
