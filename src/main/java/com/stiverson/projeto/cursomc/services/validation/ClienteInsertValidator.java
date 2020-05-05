92502c5825be8941647a6f00c02164acf84f87db
Stiverson <stiverson.machado@gmail.com>
2020-05-05 13:31:18 -0300

    Validação customizada de e-mail não repetido

--- src/main/java/com/stiverson/projeto/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/stiverson/projeto/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.stiverson.projeto.cursomc.domain.Cliente;
 import com.stiverson.projeto.cursomc.domain.enums.TipoCliente;
 import com.stiverson.projeto.cursomc.dto.ClienteNewDTO;
+import com.stiverson.projeto.cursomc.repositories.ClienteRepository;
 import com.stiverson.projeto.cursomc.resources.exception.FieldMessage;
 import com.stiverson.projeto.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired 
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -31,6 +39,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			
+			list.add(new FieldMessage("email","Esse E-mail já esta cadastrado"));
+			
+		}
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
