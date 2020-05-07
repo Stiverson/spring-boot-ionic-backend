16b0b82dd3471b5f00793e8d3e4647333906e840
Stiverson <stiverson.machado@gmail.com>
2020-05-07 15:41:32 -0300

    Subtotal e total de pedido adicionados

--- src/main/java/com/stiverson/projeto/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/stiverson/projeto/cursomc/repositories/ProdutoRepository.java
@@ -5,17 +5,18 @@ package com.stiverson.projeto.cursomc.repositories;
 
 import java.util.List;
 
+
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.stiverson.projeto.cursomc.domain.Categoria;
 import com.stiverson.projeto.cursomc.domain.Produto;
 
 @Repository
+
 public interface ProdutoRepository extends JpaRepository<Produto ,Integer> {
 
 	
