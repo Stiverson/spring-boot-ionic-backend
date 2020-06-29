f9cff54ef5c9643b2edddfbb3c1f3758439d1235
Stiverson <stiverson.machado@gmail.com>
2020-06-29 12:10:54 -0300

    E-mail HTML

--- src/main/java/com/stiverson/projeto/cursomc/services/MockEmailService.java
+++ src/main/java/com/stiverson/projeto/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.stiverson.projeto.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -17,6 +19,15 @@ public class MockEmailService extends AbstractEmailService {
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 		
+	}
+
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		
+		LOG.info("Simulando envio de e-mail HTML ...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
 	}
 
 }
